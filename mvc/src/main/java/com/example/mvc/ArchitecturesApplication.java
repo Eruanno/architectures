93420f666f046e8ecc7316eca166997b8434c91c
Eruanno <maciek.baginski@wp.pl>
2025-02-28 02:41:56 +0100

    init multi projest init subproject for mvc

--- mvc/src/main/java/com/example/mvc/ArchitecturesApplication.java
+++ mvc/src/main/java/com/example/mvc/ArchitecturesApplication.java
@@ -1,4 +1,4 @@
-package com.example.architectures;
+package com.example.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class ArchitecturesApplication {
     public static void main(String[] args) {
         SpringApplication.run(ArchitecturesApplication.class, args);
     }
-
 }
