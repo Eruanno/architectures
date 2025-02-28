94b3c3e9f25a4a540a2de3c532f4803a8fe477bf
Eruanno <maciek.baginski@wp.pl>
2025-02-28 01:15:58 +0100

    init repository

---
+++ src/main/java/com/example/architectures/ArchitecturesApplication.java
@@ -0,0 +1,13 @@
+package com.example.architectures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitecturesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchitecturesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/architectures/ArchitecturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.architectures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitecturesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
